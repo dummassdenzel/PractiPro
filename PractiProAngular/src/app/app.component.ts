83991bd1fb1949e26355b16458dcbab7cc523f3e
RoescenAbiePajaro <157484846+RoescenAbiePajaro@users.noreply.github.com>
2024-03-04 09:18:34 -0600

    student login
    di pa maayos

--- PractiProAngular/src/app/app.component.ts
+++ PractiProAngular/src/app/app.component.ts
@@ -5,11 +5,12 @@ import { OnInit } from '@angular/core';
 import { initFlowbite } from 'flowbite';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TestComponent, SidebarComponent, DashboardComponent],
+  imports: [RouterOutlet, TestComponent, SidebarComponent, DashboardComponent,LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
