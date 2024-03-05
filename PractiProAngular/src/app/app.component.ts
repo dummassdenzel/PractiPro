52b942c46ef4822ea36c856a2515a4798fbfda7e
Denzel Manz <114593274+dummassdenzel@users.noreply.github.com>
2024-03-05 12:31:48 +0800

    import dashboard into app component

--- PractiProAngular/src/app/app.component.ts
+++ PractiProAngular/src/app/app.component.ts
@@ -3,14 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { TestComponent } from './test/test.component';
 import { OnInit } from '@angular/core';
 import { initFlowbite } from 'flowbite';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TestComponent, DashboardComponent, LoginComponent],
+  imports: [RouterOutlet, TestComponent, LoginComponent, DashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
