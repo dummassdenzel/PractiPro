f7af470da51897dcb8cb19ede805a54f24e3a460
Denzel Manz <114593274+dummassdenzel@users.noreply.github.com>
2024-03-05 12:19:18 +0800

    add login component

--- PractiProAngular/src/app/app.component.ts
+++ PractiProAngular/src/app/app.component.ts
@@ -3,23 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { TestComponent } from './test/test.component';
 import { OnInit } from '@angular/core';
 import { initFlowbite } from 'flowbite';
-<<<<<<< HEAD
 import { DashboardComponent } from './dashboard/dashboard.component';
-
-=======
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
->>>>>>> 83991bd1fb1949e26355b16458dcbab7cc523f3e
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-<<<<<<< HEAD
-  imports: [RouterOutlet, TestComponent, DashboardComponent],
-=======
-  imports: [RouterOutlet, TestComponent, SidebarComponent, DashboardComponent,LoginComponent],
->>>>>>> 83991bd1fb1949e26355b16458dcbab7cc523f3e
+  imports: [RouterOutlet, TestComponent, DashboardComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
